576661b25e348aff8fc63a24a510f05a46d7d8cb
dragonechoi <vasco1379@naver.com>
2024-01-22 14:59:37 +0900

    Splash화면 설계 및 handler 이용 1.5초 (1500ms) LoginActivity 화면전환 구현 완료 or LoginActivity 화면 디자인 구현 설계 완료

--- app/src/main/java/com/sinina/sininacake/ui/LoginActivity.kt
+++ app/src/main/java/com/sinina/sininacake/ui/LoginActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinina.sininacake.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
     }
 }
